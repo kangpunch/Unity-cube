74c296c935f4f29ef2b3fee24573126fb8297161
kangpunch <swjy333@gmail.com>
2021-04-13 21:08:58 +0900

    스피어생성!

--- Assets/Sphere.cs
+++ Assets/Sphere.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cube : MonoBehaviour
+public class Sphere : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
